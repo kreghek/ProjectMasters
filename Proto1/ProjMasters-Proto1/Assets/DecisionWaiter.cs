12482b17ac30404533d958840bd52ed098715462
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-04-01 21:27:46 +0700

    Ui for decisions

---
+++ Proto1/ProjMasters-Proto1/Assets/DecisionWaiter.cs
@@ -0,0 +1,19 @@
+﻿using Assets.BL;
+
+using UnityEngine;
+
+public class DecisionWaiter : MonoBehaviour
+{
+    public DecisionHandler DecisionHandler;
+
+    public void Update()
+    {
+        if (Player.WaitForDecision != null)
+        {
+            if (!DecisionHandler.gameObject.activeSelf)
+            {
+                DecisionHandler.Init(Player.WaitForDecision);
+            }
+        }
+    }
+}
