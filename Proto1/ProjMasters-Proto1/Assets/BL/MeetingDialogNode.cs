1fefa6f270e35124531f3ba465ee0a640fcd4606
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-04-10 16:33:45 +0700

    WIP Develop meeting scene

---
+++ Proto1/ProjMasters-Proto1/Assets/BL/MeetingDialogNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.BL
+{
+    class MeetingDialogNode
+    {
+        public string SpeakerName { get; set; }
+        public string Text { get; set; }
+        public bool IsEndNode { get; set; }
+    }
+
+    class MeetingAnswer
+    { 
+        public string Text { get; set; }
+
+        public Person AddNewPerson { get; set; }
+    }
+}
