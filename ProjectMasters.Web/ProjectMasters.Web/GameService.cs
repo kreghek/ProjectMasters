502cf50fd280ad83b4b4cf067e0f0630f9a4cdbc
Куркутов П.А <kurkutovpa@tomskasu.ru>
2021-05-08 10:38:09 +0700

    Init

---
+++ ProjectMasters.Web/ProjectMasters.Web/GameService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectMasters.Web
+{
+    public class GameService
+    {
+        private ConcurrentBag<Game> _games = new ConcurrentBag<Game>();
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                foreach (var game in _games.ToArray())
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    await game.UpdateAsync();
+                }
+            }
+        }
+
+        public void AddGame(string user)
+        {
+            var game = new Game
+            {
+                User = user
+            };
+
+            _games.Add(game);
+        }
+    }
+
+    public class Game
+    { 
+        public string User { get; set; }
+
+        internal Task UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectMasters.Web/ProjectMasters.Web/Hubs/GameHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace ProjectMasters.Web.Hubs
+{
+    public class GameHub: Hub
+    {
+        private readonly GameService _gameService;
+
+        public GameHub(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        public Task StartGameAsync(string user)
+        {
+            return Task.Run(() =>
+            {
+                _gameService.AddGame(user);
+            });
+        }
+    }
+}
