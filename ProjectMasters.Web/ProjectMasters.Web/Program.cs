3982cb8b3250d112a8da1f07804778343ae242b5
Куркутов П.А <kurkutovpa@tomskasu.ru>
2021-05-08 13:48:33 +0700

    Исправление после слияния

--- ProjectMasters.Web/ProjectMasters.Web/Program.cs
+++ ProjectMasters.Web/ProjectMasters.Web/Program.cs
@@ -18,12 +18,6 @@ namespace ProjectMasters.Web
 
     public class Program
     {
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        }
-
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
