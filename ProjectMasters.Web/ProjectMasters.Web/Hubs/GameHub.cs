68e93cbdfee0fd697fdf6204a865b4c2dc81d887
Pavel Kurkutov <kurkutovpa@gmail.com>
2021-05-14 02:30:51 +0000

    [Bot] Automated dotnet-format update

--- ProjectMasters.Web/ProjectMasters.Web/Hubs/GameHub.cs
+++ ProjectMasters.Web/ProjectMasters.Web/Hubs/GameHub.cs
@@ -50,8 +50,8 @@
 
             var lineToGetQueueIndecies = gameState.Project.Lines.SingleOrDefault(x => x.Id == lineId);
             if (lineToGetQueueIndecies is null)
-                // Не нашли линию проекта.
-                // Это значит, что убили последнего монстра и линия была удалена.
+            // Не нашли линию проекта.
+            // Это значит, что убили последнего монстра и линия была удалена.
             {
                 return;
             }
